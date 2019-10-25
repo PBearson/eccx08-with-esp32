2c407b78e75dab5402e9491dcdd7ea752831bfb9
Bryan Pearson <bpearson@stetson.edu>
2019-10-25 06:38:52 -0700

    Cleaned dir

--- main/main.c
+++ main/main.c
@@ -1,15 +1,15 @@
-/* Hello World Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "cryptoauthlib.h"
 #include "driver/i2c.h"
 #include "crypto_settings.h"
+#include "freertos/task.h"
+
+void check_error(int ret)
+{
+	if(ret == 0) return;
+	printf("There is a problem. Error code %d. Refer here for more information:\n%s\n", ret, "https://github.com/MicrochipTech/cryptoauthlib/blob/master/lib/atca_status.h");
+	while(true) vTaskDelay(1000 / portTICK_PERIOD_MS);
+}
 
 /*
  * Start here
@@ -18,7 +18,14 @@ void app_main()
 {
 	ATCAIfaceCfg cfg = atca_cfg_init();	
 	i2c_init();
-	int ret = atcab_init(&cfg);	
-	if (ret == 0) printf("Your cryptochip is successfully connected!\n");
-	else printf("There is a problem. Error code %d. Refer here for more information:\n%s\n", ret, "https://github.com/MicrochipTech/cryptoauthlib/blob/master/lib/atca_status.h");
+	check_error(atcab_init(&cfg));	
+	printf("Your cryptochip is successfully connected!\n\n");
+
+	uint8_t config_zone[128];
+
+	check_error(atcab_read_config_zone(config_zone));
+
+	printf("Config zone:\n");
+	for(int i = 0; i < 127; i++) printf("%u, ", config_zone[i]);
+	printf("%u\n", config_zone[127]);
 }
